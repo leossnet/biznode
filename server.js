eb0dc534d92a68207f104fac2449a6eced043fb0
leossnet <leossnet@gmail.com>
2020-08-20 20:08:41 +0500

    возвращена базовая схема импорта модулей

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 /**
  * Серверный модуль веб-сервера
  */
-import { createServer } from 'http';
-import { readFile, createReadStream } from 'fs';
+const http = require ('http');
+const fs = require('fs');
 
 const PORT = 80;
 
 /**
  * Инициализация и запуск сервера приложения в контексте node.js
  */
-createServer((request, response) => {
+http.createServer((request, response) => {
     const filePath = request.url.substr(1);
-    readFile(filePath, (error, data) => {
+    fs.readFile(filePath, (error, data) => {
         if ( error ) {
             response.statusCode = 404;
             response.end("File not found...");
         }
         else {
-            createReadStream(filePath).pipe(response);
+            fs.createReadStream(filePath).pipe(response);
         }
     });
 }).listen(PORT, () => {
