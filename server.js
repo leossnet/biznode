1a73ab8fc5cf5e2f7f265410214f649a08868011
leossnet <leossnet@gmail.com>
2020-09-05 15:57:28 +0500

    bn-2 сервер переписан в ооп-стиле

--- server.js
+++ server.js
@@ -7,19 +7,44 @@ const fs = require('fs');
 const PORT = 80;
 
 /**
- * Инициализация и запуск сервера приложения в контексте node.js
+ * Класс http-сервера
  */
-http.createServer((request, response) => {
-    const filePath = request.url.substr(1);
-    fs.readFile(filePath, (error, data) => {
-        if ( error ) {
-            response.statusCode = 404;
-            response.end("File not found...");
-        }
-        else {
-            fs.createReadStream(filePath).pipe(response);
-        }
-    });
-}).listen(PORT, () => {
-    console.log(`Server started at ${PORT} port...`);
-});
+class Server {
+    #server;
+
+    /**
+     * Конструктор http-сервера
+     */
+    constructor() {
+        this.#server = http.createServer((request, response) => {
+            const filePath = request.url.substr(1);
+            this.#readFile(filePath, response);
+        });
+
+        this.#server.listen(PORT, () => {
+            console.log(`Server started at ${PORT} port...`);
+        });
+    }
+
+    /**
+     * Чтение содержимого файла с возвратом прочитанного значения
+     * @param {String} filePath - полное имя файла
+     * @param {String} response - выходной поток
+     */
+    #readFile(filePath, response) {
+        const headParams = {'Content-Type': 'text/plain;charset=utf-8'};
+        fs.readFile(filePath, (error, data) => {
+            if (error) {
+                response.writeHead(400, headParams);
+                response.end("Файл не найден...");
+            }
+            else {
+                response.writeHead(200, headParams);
+                fs.createReadStream(filePath).pipe(response);
+            }
+        });
+    }
+}
+
+// создание и запуск http-сервера
+new Server();
